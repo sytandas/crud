d0acb69e4426321461bd214cbdc6cefafec5ced7
sayantan3 <sayantand975@gmail.com>
2022-07-26 06:45:32 +0530

    psql

--- crud.py
+++ crud.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
-import sys
 import psycopg2 
 
-"""
-try:
-    conn = psycopg2.connect("dbname='temp' use='dbuser' host='localhost' password='dbpass'")
-except:
-    print('I am unable to connect to the database')
-"""
+#connection conn = psycopg2.connect("dbname=testdb user=postgres password=password")
+conn=psycopg2.connect(
+  database="mydb",
+  user="sayantandas",
+  host="/tmp/",
+  password="123"
+)
+#creating a cursor object using the cursor() method - what communicate with db
+cur = conn.cursor()
 
-#number = input("1.create, 2.read, 3.update, 4.delete/modify: ")
-print("1.create, 2.read, 3.update, 4.delete/modify ")
+#executing an MYSQL function using the execute() method
+cur.execute("SELECT version();")
+
+record = cur.fetchone()
+print("connected to- ", record,"\n")
+
+cur.close()
+conn.close()
+print("Postgre connection is closed")
+
+number = int(input("1.create, 2.read, 3.update, 4.delete/modify\nchoose:: "))
 
 def create():
     return "create"
@@ -38,7 +49,4 @@ switcher = {
 def selectFunction(num):
     return switcher.get(num, default)()
 
-number = int(input("choose:: "))
 print(selectFunction(number))
-
-
