2c79b50fe17ade9c7ffce2ab967a17136a02a081
sayantan3 <sayantand975@gmail.com>
2022-09-01 23:27:54 +0530

    prac

---
+++ javascript/index.js
@@ -0,0 +1,36 @@
+// require to pass the express module; having built-in body-parser middleware, 
+// set up app and port variables
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const db = require('./queries');
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+// GET request
+
+app.get ('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUsersById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+// Setting up app to listen on the port 
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+
