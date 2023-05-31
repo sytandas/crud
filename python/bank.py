eda3fe7d258d0240a1aa7cadf7c4934c6b6c92b7
sayantan3 <sayantand975@gmail.com>
2023-05-31 11:13:48 +0530

    boring

---
+++ python/bank.py
@@ -0,0 +1,32 @@
+class BankAccount:
+    def __init__ (self, account_number, balance, date_opening, customer_name):
+        self.account_number = account_number
+        self.balance = balance
+        self.date_opening = date_opening
+        self.customer_name = customer_name
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"₹‎{amount} has been deposited in your account")
+    def withdraw(self, amount):
+        if amount >= self.balance:
+            print("insufficient balance")
+        else:
+            print(f"balance: {balance}")
+    
+    def check_balance(self):
+        print(f"current balance is: {self.balance}")
+
+    def acc_info(self):
+        print(f"account number: {self.account_number}, name: {self.customer_name}, balance: {self.balance}")
+        print("-------------------------------------------------------------------------------------------")
+
+ba_1 = BankAccount("P00001", 5000000, "31-05-2023", "Abhay")
+ba_2 = BankAccount("P00010", 7000000, "11-05-2023", "Anirudh")
+ba_3 = BankAccount("P00011", 9000000, "06-05-2023", "Abhinav")
+
+ba_1.acc_info()
+ba_2.acc_info()
+
+ba_1.deposit(200)
+ba_1.acc_info()
